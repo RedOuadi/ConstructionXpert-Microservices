5ae76441c8dab9186519425ee379e5bf9cb75665
Reda Ouadi <redaouadi34@gmail.com>
2024-08-29 10:32:02 +0100

    Changement de /ProjetServiceApplicationTests.java

--- projet-service/src/test/java/projetservice/ProjetServiceApplicationTests.java
+++ projet-service/src/test/java/projetservice/ProjetServiceApplicationTests.java
@@ -1,13 +1,13 @@
-package projetservice;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ProjetServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package projetservice;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ProjetServiceApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
